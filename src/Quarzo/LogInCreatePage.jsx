e02e919b86a9f2af6253290688a013a9c4f97b01
Eduardo <tkd13000@gmail.com>
2024-09-10 07:30:13 -0600

    content adjusted

--- src/Quarzo/LogInCreatePage.jsx
+++ src/Quarzo/LogInCreatePage.jsx
@@ -3,11 +3,12 @@ import styles from "./Log.module.css"
 function LogInCreatePage()
 {
     return(
-        <body>
+        <div className={styles.pageContainer}>
             <img className = {styles.containerBackgroundImage} src="../Quarzo.webp" alt="" />
             <main className = {styles.containerMain}>
+                <p className = {styles.containerMain_title}>Quarzo</p>
             </main>
-        </body>
+        </div>
 
         
     );

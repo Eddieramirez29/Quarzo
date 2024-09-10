9e43be6bb90010dcdc15f4fb47e6e38d2cb928c4
Eduardo <tkd13000@gmail.com>
2024-09-10 08:12:26 -0600

    On process

--- src/Quarzo/LogInCreatePage.jsx
+++ src/Quarzo/LogInCreatePage.jsx
@@ -7,6 +7,33 @@ function LogInCreatePage()
             <img className = {styles.containerBackgroundImage} src="../Quarzo.webp" alt="" />
             <main className = {styles.containerMain}>
                 <p className = {styles.containerMain_title}>Quarzo</p>
+                <div className = {styles.containerMain_subContainer}>
+                <div className={styles.containerMain_register}>Hi</div>
+                <div className={styles.containerMain_logIn}>
+                <div className="form-container">
+      <form>
+        <label htmlFor="email">Correo Electrónico:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+        />
+        
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+        />
+        
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+    </div>
+                    
+                </div>
+                </div>
             </main>
         </div>
 

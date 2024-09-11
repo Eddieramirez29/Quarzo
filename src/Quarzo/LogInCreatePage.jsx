e0bbabd69e163af31af2fcf968a300da9f86803f
Eduardo <tkd13000@gmail.com>
2024-09-11 06:14:46 -0600

    Log in for on process

--- src/Quarzo/LogInCreatePage.jsx
+++ src/Quarzo/LogInCreatePage.jsx
@@ -9,31 +9,19 @@ function LogInCreatePage()
                 <p className = {styles.containerMain_title}>Quarzo</p>
                 <div className = {styles.containerMain_subContainer}>
                 <div className={styles.containerMain_register}>Hi</div>
-                <div className={styles.containerMain_logIn}>
-                <div className="form-container">
-      <form>
-        <label htmlFor="email">Correo Electrónico:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-        />
-        
-        <label htmlFor="password">Contraseña:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          required
-        />
-        
-        <button type="submit">Iniciar Sesión</button>
-      </form>
-    </div>
-                    
-                </div>
-                </div>
+        <div className={styles.containerMain_logIn}>
+        <div className="form-container">
+            <form>
+              <label htmlFor="email" className = {styles.containerMain_EmailLabel}>Correo Electrónico</label>
+                <input type="email" placeholder="Tu correo electrónico" className = {styles.containerMain_Email} name="email"required/>
+              <label htmlFor="password" className = {styles.containerMain_EmailPassowrd}>Contraseña</label>
+                <input type="password" placeholder="Tu contraseña" className = {styles.containerMain_Password} name="password" required />
+                <a href="" className = {styles.containerMain_Forgotten}>¿Has olvidado la contraseña</a>
+              <button className = {styles.containerMain_ButtonLogIn} type="submit">Iniciar Sesión</button>
+            </form>
+        </div>
+        </div>
+        </div>
             </main>
         </div>
 

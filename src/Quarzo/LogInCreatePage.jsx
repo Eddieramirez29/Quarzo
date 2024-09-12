3f183149e0912b9907002c53060293fc70a3100f
Eduardo <tkd13000@gmail.com>
2024-09-12 06:39:08 -0600

    Container for Login is finished

--- src/Quarzo/LogInCreatePage.jsx
+++ src/Quarzo/LogInCreatePage.jsx
@@ -4,19 +4,22 @@ function LogInCreatePage()
 {
     return(
         <div className={styles.pageContainer}>
-            <img className = {styles.containerBackgroundImage} src="../Quarzo.webp" alt="" />
+            <img className = {styles.containerBackgroundImage} src="../Quarzoi.webp" alt="" />
             <main className = {styles.containerMain}>
                 <p className = {styles.containerMain_title}>Quarzo</p>
                 <div className = {styles.containerMain_subContainer}>
                 <div className={styles.containerMain_register}>Hi</div>
         <div className={styles.containerMain_logIn}>
+          <p>Iniciar Sesión</p>
+        <hr className={styles.containerMain_logIn_upperbar} />
+
         <div className="form-container">
             <form>
               <label htmlFor="email" className = {styles.containerMain_EmailLabel}>Correo Electrónico</label>
                 <input type="email" placeholder="Tu correo electrónico" className = {styles.containerMain_Email} name="email"required/>
               <label htmlFor="password" className = {styles.containerMain_EmailPassowrd}>Contraseña</label>
                 <input type="password" placeholder="Tu contraseña" className = {styles.containerMain_Password} name="password" required />
-                <a href="" className = {styles.containerMain_Forgotten}>¿Has olvidado la contraseña</a>
+                <a href="" className = {styles.containerMain_Forgotten}>¿Has olvidado la contraseña?</a>
               <button className = {styles.containerMain_ButtonLogIn} type="submit">Iniciar Sesión</button>
             </form>
         </div>

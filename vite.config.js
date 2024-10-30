bfd025aee86cd990c9ccd05b5cb376d717bf9b1a
Eduardo <tkd13000@gmail.com>
2024-10-30 07:17:51 -0600

    Nothing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Quarzo/' // Cambia 'quarzo' por el nombre real de tu repositorio
 })
